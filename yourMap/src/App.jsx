d755de11025b0d103d215522f792e18ed409d467
Heela Bangash <heelabangash@gmail.com>
2024-09-23 01:00:48 -0400

    add zoom and pan, style buttons, style map

--- yourMap/src/App.jsx
+++ yourMap/src/App.jsx
@@ -1,35 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-import {
-	ComposableMap,
-	Geographies,
-	Annotation,
-	Geography,
-} from "react-simple-maps";
-import Map from "./components/Map/Map";
-function App() {
-	// const coords = [{ DEU: "10.451526 51.165691" }];
-	// const highlighted = [
-	// 	"BRA",
-	// 	"VNM",
-	// 	"COL",
-	// 	"IDN",
-	// 	"ETH",
-	// 	"HND",
-	// 	"IND",
-	// 	"UGA",
-	// 	"MEX",
-	// ];
-	// const [currentCountry, setCurrentCountry] = useState("");
-	// const [hoverCountry, setHoverCountry] = useState("");
+import "./App.scss";
 
-	// const [coord, setCoord] = useState({
-	// 	center: [0, 0],
-	// 	zoom: 1,
-	// 	currentCountry: null,
-	// });
+import Map from "./components/Map/Map";
+
+function App() {
+	const [currentCountry, setCurrentCountry] = useState("");
+
 	// const [people, selectPeople] = useState(["Alicia, Katarina, Tami"]);
 	// const [flag, setFlag] = useState(false);
 	// const selectCountry = (geo) => {
@@ -49,22 +27,15 @@ function App() {
 	// 		selectPeople(["none"]);
 	// 	}
 	// };
-	// const showCountry = (geo) => setHoverCountry(geo.properties.name);
-	// const hideCountry = () => setHoverCountry("");
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1 className="logo">yourMap</h1>
+			<Map
+				className="map-container"
+				currentCountry={currentCountry}
+				setCurrentCountry={setCurrentCountry}
+			></Map>
 
-			<Map></Map>
 			<div className="card">placeholder navbar</div>
 		</>
 	);
--- yourMap/src/components/Map/Map.jsx
+++ yourMap/src/components/Map/Map.jsx
@@ -5,39 +5,39 @@ import {
 	Geographies,
 	Annotation,
 	Geography,
+	ZoomableGroup,
 } from "react-simple-maps";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "/vite.svg";
 
-function Map() {
-	const coords = [{ DEU: "10.451526 51.165691" }];
-	const highlighted = [
-		"BRA",
-		"VNM",
-		"COL",
-		"IDN",
-		"ETH",
-		"HND",
-		"IND",
-		"UGA",
-		"MEX",
-	];
-	const [currentCountry, setCurrentCountry] = useState("");
+function Map({ currentCountry, setCurrentCountry }) {
+	// const coords = [{ DEU: "10.451526 51.165691" }];
+	// const highlighted = [
+	// 	"BRA",
+	// 	"VNM",
+	// 	"COL",
+	// 	"IDN",
+	// 	"ETH",
+	// 	"HND",
+	// 	"IND",
+	// 	"UGA",
+	// 	"MEX",
+	// ];
 	const [hoverCountry, setHoverCountry] = useState("");
-
 	const [coord, setCoord] = useState({
-		center: [0, 0],
-		zoom: 1,
-		currentCountry: null,
+		center: [18, 10],
+		zoom: 1.7,
 	});
 	const [people, selectPeople] = useState(["Alicia, Katarina, Tami"]);
 	const [flag, setFlag] = useState(false);
 	const selectCountry = (geo) => {
 		// alert(geo.id);
 		// console.log(geo);
-		console.log(geo);
-		console.log(geo.properties.name);
+		// console.log(geo);
+		// console.log(geo.properties.name);
 		// console.log(geo.geometry.coordinates);
 		setCurrentCountry(geo.properties.name);
-		console.log(flag);
+		// console.log(flag);
 		// alert(geo.properties.name);
 		flag
 			? selectPeople(["Justin, Henry, Emma"])
@@ -52,50 +52,84 @@ function Map() {
 
 	return (
 		<>
-			<div className="card">
-				<button onClick={() => setCurrentCountry("")}>
-					{currentCountry ? `You're in ${currentCountry}!` : "Welcome!"}
-				</button>
-				{hoverCountry && <button>{hoverCountry}</button>}
-			</div>
-
-			<ComposableMap
-				className="map-container"
-				projection="geoEqualEarth"
-				projectionConfig={{
-					// rotate: [-10.0, -52.0, 0],
-					// center: [2, 46],
-					scale: 150,
-				}}
-			>
-				<Geographies className="map" geography="/features.json">
-					{({ geographies }) =>
-						geographies.map(
-							(geo) => (
-								// <div key={geo.rsmKey}>
-								// console.log(geo.rsmKey) || (
-								<Geography
-									className={
-										geo.properties.name == currentCountry
-											? "--active"
-											: "country"
-									}
-									key={geo.rsmKey}
-									geography={geo}
-									fill="#F6F0E9"
-									onClick={() => selectCountry(geo)}
-									onMouseEnter={() => showCountry(geo)}
-									onMouseLeave={() => hideCountry()}
-								/>
-							)
-							// {geo.id === currentCountry ? "url('#lines')" :
-							// )
-							// </div>
-						)
-					}
-				</Geographies>
-
-				{/* <Annotation
+			<div className="map">
+				<div className="map__options">
+					<div className="title">
+						<div className="logos">
+							<a href="https://vitejs.dev" target="_blank">
+								<img src={viteLogo} className="logo" alt="Vite logo" />
+							</a>
+							<a href="https://react.dev" target="_blank">
+								<img src={reactLogo} className="logo react" alt="React logo" />
+							</a>
+						</div>
+						<h1 className="app-logo">yourMap</h1>
+					</div>
+					<div className="map__buttons">
+						<button
+							onClick={() =>
+								setCoord({
+									center: [18, 10],
+									zoom: 1.7,
+								})
+							}
+						>
+							{"Recenter"}
+						</button>
+						<button onClick={() => setCurrentCountry("")}>
+							{currentCountry
+								? `You're in ${currentCountry}!`
+								: "Welcome! Click here anytime to deselect"}
+						</button>
+						{hoverCountry && (
+							<button className="country-label">{hoverCountry}</button>
+						)}
+					</div>
+				</div>
+				<ComposableMap
+					className="map__container"
+					projection="geoEqualEarth"
+					projectionConfig={{
+						// rotate: [-10.0, -52.0, 0],
+						// center: [2, 46],
+						scale: 150,
+					}}
+				>
+					<ZoomableGroup
+						center={coord.center}
+						zoom={coord.zoom}
+						onMoveEnd={({ coordinates, zoom }) => {
+							console.log(coordinates, zoom);
+							setCoord({ center: coordinates, zoom: zoom });
+						}}
+					>
+						<Geographies className="map" geography="/features.json">
+							{({ geographies }) =>
+								geographies.map(
+									(geo) => (
+										// <div key={geo.rsmKey}>
+										// console.log(geo.rsmKey) || (
+										<Geography
+											className={
+												geo.properties.name == currentCountry
+													? "--active"
+													: "country"
+											}
+											key={geo.rsmKey}
+											geography={geo}
+											fill="#F6F0E9"
+											onClick={() => selectCountry(geo)}
+											onMouseEnter={() => showCountry(geo)}
+											onMouseLeave={() => hideCountry()}
+										/>
+									)
+									// {geo.id === currentCountry ? "url('#lines')" :
+									// )
+									// </div>
+								)
+							}
+						</Geographies>
+						{/* <Annotation
 					subject={[2.3522, 48.8566]}
 					dx={-90}
 					dy={-30}
@@ -109,7 +143,9 @@ function Map() {
 						{"Paris"}
 					</text>
 				</Annotation> */}
-			</ComposableMap>
+					</ZoomableGroup>
+				</ComposableMap>
+			</div>
 			{people[0] == "none" ? (
 				<h2> ˙◠˙</h2>
 			) : (
