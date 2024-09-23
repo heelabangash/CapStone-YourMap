a05ce2ec1dd5a5037525977f1fa6ab5c4c115dea
Heela Bangash <heelabangash@gmail.com>
2024-09-22 20:36:05 -0400

    move map to component and scale

--- yourMap/src/App.jsx
+++ yourMap/src/App.jsx
@@ -8,37 +8,49 @@ import {
 	Annotation,
 	Geography,
 } from "react-simple-maps";
-
+import Map from "./components/Map/Map";
 function App() {
-	const coords = [{ DEU: "10.451526 51.165691" }];
-	const highlighted = [
-		"BRA",
-		"VNM",
-		"COL",
-		"IDN",
-		"ETH",
-		"HND",
-		"IND",
-		"UGA",
-		"MEX",
-	];
-	const [currentCountry, setCurrentCountry] = useState("");
-	const [coord, setCoord] = useState({
-		center: [0, 0],
-		zoom: 1,
-		currentCountry: null,
-	});
-	const [count, setCount] = useState(0);
+	// const coords = [{ DEU: "10.451526 51.165691" }];
+	// const highlighted = [
+	// 	"BRA",
+	// 	"VNM",
+	// 	"COL",
+	// 	"IDN",
+	// 	"ETH",
+	// 	"HND",
+	// 	"IND",
+	// 	"UGA",
+	// 	"MEX",
+	// ];
+	// const [currentCountry, setCurrentCountry] = useState("");
+	// const [hoverCountry, setHoverCountry] = useState("");
 
-	const selectCountry = (geo) => {
-		// alert(geo.id);
-		console.log(geo);
-		console.log(geo.id);
-		console.log(geo.properties.name);
-		console.log(geo.geometry.coordinates);
-		setCurrentCountry(geo.properties.name);
-		// alert(geo.properties.name);
-	};
+	// const [coord, setCoord] = useState({
+	// 	center: [0, 0],
+	// 	zoom: 1,
+	// 	currentCountry: null,
+	// });
+	// const [people, selectPeople] = useState(["Alicia, Katarina, Tami"]);
+	// const [flag, setFlag] = useState(false);
+	// const selectCountry = (geo) => {
+	// 	// alert(geo.id);
+	// 	// console.log(geo);
+	// 	console.log(geo);
+	// 	console.log(geo.properties.name);
+	// 	// console.log(geo.geometry.coordinates);
+	// 	setCurrentCountry(geo.properties.name);
+	// 	console.log(flag);
+	// 	// alert(geo.properties.name);
+	// 	flag
+	// 		? selectPeople(["Justin, Henry, Emma"])
+	// 		: selectPeople(["Jenani, Mingxia, Heela"]);
+	// 	setFlag(!flag);
+	// 	if (geo.id == "BRA" || geo.id == "AUS") {
+	// 		selectPeople(["none"]);
+	// 	}
+	// };
+	// const showCountry = (geo) => setHoverCountry(geo.properties.name);
+	// const hideCountry = () => setHoverCountry("");
 
 	return (
 		<>
@@ -51,62 +63,9 @@ function App() {
 				</a>
 			</div>
 			<h1 className="logo">yourMap</h1>
-			<div className="card">
-				<button onClick={() => setCurrentCountry("")}>
-					{currentCountry ? `You're in ${currentCountry}!` : "Welcome!"}
-				</button>
-			</div>
 
-			<ComposableMap
-				className="map-container"
-				projectionConfig={
-					{
-						// rotate: [-10.0, -52.0, 0],
-						// center: [2, 46],
-						// scale: 1100,
-					}
-				}
-			>
-				{/* {console.log("something")} */}
-				<Geographies className="map" geography="/features.json">
-					{({ geographies }) =>
-						geographies.map(
-							(geo) => (
-								// <div key={geo.rsmKey}>
-								// console.log(geo.rsmKey) || (
-								<Geography
-									className={
-										geo.properties.name == currentCountry
-											? "--active"
-											: "country"
-									}
-									key={geo.rsmKey}
-									geography={geo}
-									fill="#F6F0E9"
-									onClick={() => selectCountry(geo)}
-								/>
-							)
-							// {geo.id === currentCountry ? "url('#lines')" :
-							// )
-							// </div>
-						)
-					}
-				</Geographies>
-				{/* <Annotation
-					subject={[2.3522, 48.8566]}
-					dx={-90}
-					dy={-30}
-					connectorProps={{
-						stroke: "#FF5533",
-						strokeWidth: 3,
-						strokeLinecap: "round",
-					}}
-				>
-					<text x="-8" textAnchor="end" alignmentBaseline="middle" fill="#F53">
-						{"Paris"}
-					</text>
-				</Annotation> */}
-			</ComposableMap>
+			<Map></Map>
+			<div className="card">placeholder navbar</div>
 		</>
 	);
 }
---
+++ yourMap/src/components/Map/Map.jsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import "./Map.scss";
+import {
+	ComposableMap,
+	Geographies,
+	Annotation,
+	Geography,
+} from "react-simple-maps";
+
+function Map() {
+	const coords = [{ DEU: "10.451526 51.165691" }];
+	const highlighted = [
+		"BRA",
+		"VNM",
+		"COL",
+		"IDN",
+		"ETH",
+		"HND",
+		"IND",
+		"UGA",
+		"MEX",
+	];
+	const [currentCountry, setCurrentCountry] = useState("");
+	const [hoverCountry, setHoverCountry] = useState("");
+
+	const [coord, setCoord] = useState({
+		center: [0, 0],
+		zoom: 1,
+		currentCountry: null,
+	});
+	const [people, selectPeople] = useState(["Alicia, Katarina, Tami"]);
+	const [flag, setFlag] = useState(false);
+	const selectCountry = (geo) => {
+		// alert(geo.id);
+		// console.log(geo);
+		console.log(geo);
+		console.log(geo.properties.name);
+		// console.log(geo.geometry.coordinates);
+		setCurrentCountry(geo.properties.name);
+		console.log(flag);
+		// alert(geo.properties.name);
+		flag
+			? selectPeople(["Justin, Henry, Emma"])
+			: selectPeople(["Jenani, Mingxia, Heela"]);
+		setFlag(!flag);
+		if (geo.id == "BRA" || geo.id == "AUS") {
+			selectPeople(["none"]);
+		}
+	};
+	const showCountry = (geo) => setHoverCountry(geo.properties.name);
+	const hideCountry = () => setHoverCountry("");
+
+	return (
+		<>
+			<div className="card">
+				<button onClick={() => setCurrentCountry("")}>
+					{currentCountry ? `You're in ${currentCountry}!` : "Welcome!"}
+				</button>
+				{hoverCountry && <button>{hoverCountry}</button>}
+			</div>
+
+			<ComposableMap
+				className="map-container"
+				projection="geoEqualEarth"
+				projectionConfig={{
+					// rotate: [-10.0, -52.0, 0],
+					// center: [2, 46],
+					scale: 150,
+				}}
+			>
+				<Geographies className="map" geography="/features.json">
+					{({ geographies }) =>
+						geographies.map(
+							(geo) => (
+								// <div key={geo.rsmKey}>
+								// console.log(geo.rsmKey) || (
+								<Geography
+									className={
+										geo.properties.name == currentCountry
+											? "--active"
+											: "country"
+									}
+									key={geo.rsmKey}
+									geography={geo}
+									fill="#F6F0E9"
+									onClick={() => selectCountry(geo)}
+									onMouseEnter={() => showCountry(geo)}
+									onMouseLeave={() => hideCountry()}
+								/>
+							)
+							// {geo.id === currentCountry ? "url('#lines')" :
+							// )
+							// </div>
+						)
+					}
+				</Geographies>
+
+				{/* <Annotation
+					subject={[2.3522, 48.8566]}
+					dx={-90}
+					dy={-30}
+					connectorProps={{
+						stroke: "#FF5533",
+						strokeWidth: 3,
+						strokeLinecap: "round",
+					}}
+				>
+					<text x="-8" textAnchor="end" alignmentBaseline="middle" fill="#F53">
+						{"Paris"}
+					</text>
+				</Annotation> */}
+			</ComposableMap>
+			{people[0] == "none" ? (
+				<h2> ˙◠˙</h2>
+			) : (
+				<>
+					<h2>We're here!</h2>{" "}
+					<ul>
+						{people.map((person, key) => (
+							<div key={key}>{person}</div>
+						))}
+					</ul>
+				</>
+			)}
+			<div className="card">
+				<button onClick={() => setCurrentCountry("")}>Add a contact</button>
+			</div>
+		</>
+	);
+}
+
+export default Map;
