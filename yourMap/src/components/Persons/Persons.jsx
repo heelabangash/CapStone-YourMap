91de3a2c4634e5947b54e821e4660363b7e24a23
Heela Bangash <heelabangash@gmail.com>
2024-09-23 06:57:15 -0400

    make call to backend api

--- yourMap/src/components/Persons/Persons.jsx
+++ yourMap/src/components/Persons/Persons.jsx
@@ -1,59 +1,110 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Persons.scss";
 import Data from "../../../public/personsList.json";
 import { v4 as uuid } from "uuid";
+import axios from "axios";
 
 function Persons({ currentCountry }) {
 	const [contactForm, showContactForm] = useState(false);
 	const addContact = () => {
 		showContactForm(!contactForm);
 	};
+	const [data, setData] = useState(Data);
+
+	let personExists = "";
+	let listPersons = "";
+
+	const fetchPersons = async () => {
+		try {
+			const response = await axios.get(`http://localhost:8080/persons`);
+			setData(response.data);
+			return response.data;
+		} catch (err) {
+			console.error("Error fetching persons", err);
+			return null;
+		}
+	};
+
+	useEffect(() => {
+		const loadPersons = async () => {
+			try {
+				const personsDetails = await fetchPersons();
+				if (personsDetails) {
+					console.log(personsDetails.persons.countries);
+					setData(personsDetails);
+
+					personExists = personsDetails.persons.countries.find(
+						(element) => element.name === currentCountry
+					);
+
+					listPersons = personExists ? personExists.persons : "";
+					// personExists
+					// 	? (listPersons = personExists.persons)
+					// 	: (listPersons = "");
+					// setData((prevData) => ({
+					// 	...prevData,
+					// 	listPersons: listPersons, // Store the list of persons in the state
+					// 	personExists: personExists, // Store personExists in the state if needed
+					// }));
+				}
+			} catch (err) {
+				console.error("Error fetching persons:", err);
+			}
+		};
+
+		loadPersons();
+	}, [currentCountry]);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log(e.target.name.value);
 		//add person to json here
 	};
 
-	let personExists = Data.persons.countries.find(
-		(element) => element.name == currentCountry
-	);
-	let listPersons = "";
-	personExists ? (listPersons = personExists.persons) : (listPersons = "");
+	listPersons = data?.persons?.countries?.find(
+		(element) => element.name === currentCountry
+	)?.persons;
 
 	return (
-		<div className="person-list">
-			<h2>
-				{currentCountry
-					? personExists
-						? `We're in ${currentCountry}!`
-						: "Do you know anyone here?"
-					: " ~ ~ Your World ~ ~"}
-			</h2>
-			<div className="person-list__persons">
-				{currentCountry
-					? listPersons
-						? listPersons.map((person) => (
-								<p key={uuid()} className="person">
-									{person.name}
-								</p>
-						  ))
-						: "˙◠˙"
-					: ""}
-			</div>
+		<>
+			{data ? (
+				<div className="person-list">
+					<h2>
+						{currentCountry
+							? listPersons
+								? `We're in ${currentCountry}!`
+								: "Do you know anyone here?"
+							: " ~ ~ Your World ~ ~"}
+					</h2>
+					<div className="person-list__persons">
+						{currentCountry
+							? listPersons
+								? listPersons.map((person) => (
+										<p key={uuid()} className="person">
+											{person.name}
+										</p>
+								  ))
+								: "˙◠˙"
+							: ""}
+					</div>
 
-			<div className="card">
-				<button onClick={() => addContact()}>Add a contact</button>
-			</div>
-			{contactForm && (
-				<form onSubmit={handleSubmit}>
-					<label className="name-label">
-						Name:
-						<input type="text" name="name" />
-					</label>
-					<input type="submit" value="Add" />
-				</form>
+					<div className="card">
+						<button onClick={addContact}>Add a contact</button>
+					</div>
+					{contactForm && (
+						<form onSubmit={handleSubmit}>
+							<label className="name-label">
+								Name:
+								<input type="text" name="name" />
+							</label>
+							<input type="submit" value="Add" />
+						</form>
+					)}
+				</div>
+			) : (
+				<p>loading</p>
 			)}
-		</div>
+		</>
 	);
 }
 
